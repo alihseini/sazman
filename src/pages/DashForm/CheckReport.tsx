f15535ddd56803f244ec66bcc970492bcaa3938e
ali hosseini <a.hosseini9@outlook.com>
2025-06-01 12:06:04 +0330

    check report page added

---
+++ src/pages/DashForm/CheckReport.tsx
@@ -0,0 +1,66 @@
+import { Button, Form, Input } from "antd";
+import React from "react";
+import { useNavigate } from "react-router";
+
+const onFinish = (values: any) => {
+  console.log("Form values:", values);
+};
+
+const validateMessages = {
+  required: "${label} is required!",
+  types: {
+    email: "${label} is not a valid email!",
+    number: "${label} is not a valid number!",
+  },
+  number: {
+    range: "${label} must be between ${min} and ${max}",
+  },
+};
+
+const CheckReport: React.FC = () => {
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <Form
+        name="nest-messages"
+        onFinish={onFinish}
+        style={{ maxWidth: 600 }}
+        validateMessages={validateMessages}
+        layout="vertical"
+      >
+        <Form.Item
+          label="شماره همراه"
+          name=""
+          rules={[
+            { required: true, message: "لطفا شماره همراه خود را وارد کنید" },
+          ]}
+        >
+          <div className="p-1 bg-gray-300 relative rounded-2xl">
+            <Input className="!border-none" />
+          </div>
+        </Form.Item>
+
+        <Form.Item
+          label="کد امنیتی"
+          name="captcha"
+          rules={[{ required: true, message: "لطفا کد امنیتی را وارد کنید" }]}
+        >
+          <div className="p-1 bg-gray-300 relative rounded-2xl w-50">
+            <Input className="!border-none" />
+          </div>
+        </Form.Item>
+        <Form.Item>
+          <Button danger onClick={() => navigate(-1)}>
+            انصراف
+          </Button>
+          <Button type="primary" htmlType="submit" className="mr-5">
+            ارسال
+          </Button>
+        </Form.Item>
+      </Form>
+    </>
+  );
+};
+
+export default CheckReport;
--- src/routes/DashFormLayout.routes.tsx
+++ src/routes/DashFormLayout.routes.tsx
@@ -2,6 +2,7 @@ import { lazy, Suspense } from "react";
 import DashFormLayout from "../layout/DashFormLayout";
 
 const Report = lazy(() => import("../pages/DashForm/Report"));
+const CheckReport = lazy(() => import("../pages/DashForm/CheckReport"));
 
 export const DashForm = {
   element: <DashFormLayout />,
@@ -12,6 +13,14 @@ export const DashForm = {
       element: (
         <Suspense fallback={<p>...Loading</p>}>
           <Report />
+        </Suspense>
+      ),
+    },
+    {
+      path: "/checkreport",
+      element: (
+        <Suspense fallback={<p>...Loading</p>}>
+          <CheckReport />
         </Suspense>
       ),
     },
