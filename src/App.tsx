fa62a451eb71d4dfae50d340c684d312a827a585
ali hosseini <a.hosseini9@outlook.com>
2025-06-07 13:23:58 +0330

    font toolbar added

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { ConfigProvider } from "antd";
 import router from "./routes/routes";
 import { RouterProvider } from "react-router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import FontSizeToolbar from "./component/common/FontSizeToolbar";
 
 const queryClient = new QueryClient();
 
@@ -15,6 +16,7 @@ function App() {
           },
         }}
       >
+        <FontSizeToolbar />
         <RouterProvider router={router} />
       </ConfigProvider>
     </QueryClientProvider>
